7f73adfb2a7a0d1c1e869ff39752a12511a979be
Arnav Gupta <arnav@codingblocks.com>
2017-07-01 16:27:48 +0530

    remove hello template
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
 
 export default {
   name: 'app',
-  components: {
-    Hello
-  }
+  components: {}
 }
 </script>
 
